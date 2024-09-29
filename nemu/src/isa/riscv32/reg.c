f744ec1a10ddcacf8e5e6fbdbe419e3fc3ea0501
tracer-ics2024 <tracer@njuics.org>
2024-09-29 17:10:39 +0800

    >  compile NEMU 231250160 左涛 Linux zeet-XiaoXinPro-16-APH8 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:10:39 up  8:15,  1 user,  load average: 1.01, 0.76, 0.72

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -31,6 +31,10 @@ word_t isa_reg_str2val(const char *s, bool *success) {
     *success = true;
     return cpu.gpr[0];
   }
+  if (strcmp(s, "pc") == 0) {
+    *success = true;
+    return cpu.pc;
+  }
   for (int i = 0; i < 32; i++) {
     if (strcmp(s, regs[i]) == 0) {
       *success = true;
