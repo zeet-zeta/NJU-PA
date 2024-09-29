03d8a679fc2d70576bc080c0ae9a1ec159bbdc79
tracer-ics2024 <tracer@njuics.org>
2024-09-29 19:07:49 +0800

    >  compile NEMU 231250160 左涛 Linux zeet-XiaoXinPro-16-APH8 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:07:49 up 10:12,  1 user,  load average: 0.52, 0.54, 0.46

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -199,7 +199,7 @@ bool check_parentheses(int p, int q) {
   return counter == 0;
 } //检查字符串是否被一对括号包围
 
-word_t eval(int p, int q) {
+int eval(int p, int q) {
   if (p > q) {
     return 0; //处理一元运算符的val1
   }
@@ -305,8 +305,8 @@ word_t eval(int p, int q) {
       }
     } //找主运算符
 
-    word_t val1 = eval(p, op_index - 1);
-    word_t val2 = eval(op_index + 1, q);
+    int val1 = eval(p, op_index - 1);
+    int val2 = eval(op_index + 1, q);
     switch (tokens[op_index].type) {
       case TK_PLUS:
         return val1 + val2;
@@ -315,7 +315,7 @@ word_t eval(int p, int q) {
       case TK_MULTIPLE:
         return val1 * val2;
       case TK_DIVIDE:
-        return (int) val1 / (int) val2;
+        return val1 / val2;
       case TK_NEGATIVE:
         return -1 * val2;
       case TK_DEREF:
@@ -341,5 +341,5 @@ word_t expr(char *e, bool *success) {
 
   /* TODO: Insert codes to evaluate the expression. */
   *success = true;
-  return eval(0, nr_token - 1);
+  return (word_t) eval(0, nr_token - 1);
 }
