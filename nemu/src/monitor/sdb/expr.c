200ae9cda1da0b298481c132a3efede8f399f9b8
tracer-ics2024 <tracer@njuics.org>
2024-10-14 23:43:38 +0800

    >  compile NEMU 231250160 左涛 Linux zeet-XiaoXinPro-16-APH8 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:43:38 up 11:10,  1 user,  load average: 0.63, 0.40, 0.37

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -325,7 +325,7 @@ double eval(int p, int q) {
       case TK_NEGATIVE:
         return -val2;
       case TK_DEREF:
-        return vaddr_read(val2, 4);
+        return vaddr_read(val2, 1);
       case TK_EQ:
         return val1 == val2;
       case TK_NEQ:
