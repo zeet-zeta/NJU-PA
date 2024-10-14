f1a51e1cf8fe67349fbed9d34d8deab7e3e2c2bd
tracer-ics2024 <tracer@njuics.org>
2024-10-14 18:42:50 +0800

    >  compile NEMU 231250160 左涛 Linux zeet-XiaoXinPro-16-APH8 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:42:50 up  6:09,  1 user,  load average: 0.62, 0.55, 0.54

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -85,7 +85,7 @@ static int cmd_p(char *args) {
     token = strchr(space_pos + 1, '\n');
     *token = '\0';
     char* expression = space_pos + 1;
-    // Log("%s %s", token, expression);
+    Log("%s %s", token, expression);
     unsigned int actual_result = expr(expression, &success);
     if (actual_result == expected_result) {
       printf("Test passed\n");
