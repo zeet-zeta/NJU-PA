c8ddab1907b3f8aad4db5ea9613e6498e986a5e0
tracer-ics2024 <tracer@njuics.org>
2024-10-14 19:01:34 +0800

    >  compile NEMU 231250160 左涛 Linux zeet-XiaoXinPro-16-APH8 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:01:34 up  6:28,  1 user,  load average: 0.76, 0.96, 0.86

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -199,7 +199,7 @@ bool check_parentheses(int p, int q) {
   return counter == 0;
 } //检查字符串是否被一对括号包围
 
-int eval(int p, int q) {
+word_t eval(int p, int q) {
   if (p > q) {
     return 0; //处理一元运算符的val1
   }
@@ -302,8 +302,8 @@ int eval(int p, int q) {
       }
     } //找主运算符
 
-    int val1 = eval(p, op_index - 1);
-    int val2 = eval(op_index + 1, q);
+    word_t val1 = eval(p, op_index - 1);
+    word_t val2 = eval(op_index + 1, q);
     switch (tokens[op_index].type) {
       case TK_PLUS:
         return val1 + val2;
@@ -318,7 +318,7 @@ int eval(int p, int q) {
         }
         return val1 / val2;
       case TK_NEGATIVE:
-        int neg = -1;
+        word_t neg = -1;
         return neg * val2;
       case TK_DEREF:
         return vaddr_read(val2, 4);
