6ff99ea66071a15fd8ce7e4eb2055861acd28aba
tracer-ics2024 <tracer@njuics.org>
2024-10-14 21:37:00 +0800

    >  compile NEMU 231250160 左涛 Linux zeet-XiaoXinPro-16-APH8 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:37:00 up  9:03,  1 user,  load average: 0.51, 0.41, 0.49

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -62,7 +62,8 @@ static int cmd_p(char *args) {
   bool success;
   unsigned int tmp = expr(args, &success);
   if (success) {
-    printf("%u 0x%.8x\n", tmp, tmp);
+    // printf("%u 0x%.8x\n", tmp, tmp);
+    printf("%u", tmp);
   } else {
     printf("err\n");
   }
