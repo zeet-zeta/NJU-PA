1955f0aed5b7ed1a8102f4d17186685c257d3865
tracer-ics2024 <tracer@njuics.org>
2024-11-02 15:35:44 +0800

    >  compile NEMU 231250160 左涛 Linux zeet-XiaoXinPro-16-APH8 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:35:44 up  5:20,  1 user,  load average: 1.23, 1.09, 0.98

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,24 +5,92 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+int my_itoa(int value, char *str) {
+    int count = 0;
+    int negative = 0;
+    if (value < 0) {
+      negative = 1;
+      value = -value;
+    }
+    do {
+      str[count++] = (value % 10) + '0';
+      value /= 10;
+    } while (value != 0);
+    if (negative) {
+      str[count++] = '-';
+    }
+    for (int i = 0; i < count / 2; i++) {
+      char tmp = str[i];
+      str[i] = str[count - i - 1];
+      str[count - i - 1] = tmp;
+    }
+    return count;
+}
+
 int printf(const char *fmt, ...) {
-  panic("Not implemented");
+  char buffer[4096];
+  va_list ap;
+  va_start(ap, fmt);
+  int ret = vsprintf(buffer, fmt, ap);
+  int len = strlen(buffer);
+  for (int i = 0; i < len; i++) {
+    putch(buffer[i]);
+  }
+  return ret;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  return vsnprintf(out, -1, fmt, ap);
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list ap;
+  va_start(ap, fmt);
+  return vsnprintf(out, -1, fmt, ap);
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list ap;
+  va_start(ap, fmt);
+  return vsnprintf(out, n, fmt, ap);
 }
 
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  int count = 0;
+  const char *p;
+  for (p = fmt; *p != '\0' && count < n - 1; p++) {
+    if (*p == '%') {
+      p++;
+      switch (*p) {
+        case 'd': {
+          int num = va_arg(ap, int);
+          count += my_itoa(num, out + count);
+          break;
+        }
+        case 's': {
+          const char *str = va_arg(ap, const char *);
+          while (*str && count < n - 1) {
+            out[count++] = *str++;
+          }
+          break;
+        }
+        case 'c': {
+          char ch = (char)va_arg(ap, int);
+          if (count < n - 1) {
+            out[count++] = ch;
+          }
+          break;
+        }
+      }
+    } else {
+      if (count < n - 1) {
+        out[count++] = *p;
+      }
+    }
+  }
+  out[count] = '\0';
+  va_end(ap);
+  return count;
 }
 
 #endif
