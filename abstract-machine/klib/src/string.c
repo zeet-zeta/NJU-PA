528148299f79a595dd5cf04f184e90b34b85c418
tracer-ics2024 <tracer@njuics.org>
2024-11-02 12:06:09 +0800

    >  compile NEMU 231250160 左涛 Linux zeet-XiaoXinPro-16-APH8 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:06:09 up  1:50,  1 user,  load average: 0.49, 0.54, 0.63

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -5,27 +5,62 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  panic("Not implemented");
+  const char *t = s;
+  while (*t) t++;
+  return t - s;
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+  char *original_dst = dst;
+  while (*src) {
+    *dst++ = *src++;
+  }
+  *dst = '\0';
+  return original_dst;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
+  char *original_dst = dst;
+  while (n > 0 && *src) {
+    *dst++ = *src++;
+    n--;
+  }
+  while (n > 0)
+  {
+    *dst++ = '\0';
+    n--;
+  }
+  return original_dst;
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+  char *original_dst = dst;
+  while (*dst) dst++;
+  while (*src) {
+    *dst++ = *src++;
+  }
+  *dst = '\0';
+  return original_dst;
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+  while (*s1 && (*s1 == *s2)) {
+    s1++;
+    s2++;
+  }
+  return *(unsigned char *)s1 - *(unsigned char *)s2;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
+  while (n > 0 && *s1 && *s2) {
+    if (*s1 != *s2) {
+      return (int)((unsigned char)(*s1) - (unsigned char)(*s2));
+    }
+    s1++;
+    s2++;
+    n--;
+  }
+  return (n > 0) ? (int)((unsigned char)(*s1) - (unsigned char)(*s2)) : 0;
 }
 
 void *memset(void *s, int c, size_t n) {
