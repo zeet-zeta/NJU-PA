f9083f8acc2d92fd3cb04165226dbc8f5124dc0f
tracer-ics2024 <tracer@njuics.org>
2024-11-02 12:07:20 +0800

    >  compile NEMU 231250160 左涛 Linux zeet-XiaoXinPro-16-APH8 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:07:20 up  1:51,  1 user,  load average: 0.68, 0.61, 0.65

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -54,13 +54,13 @@ int strcmp(const char *s1, const char *s2) {
 int strncmp(const char *s1, const char *s2, size_t n) {
   while (n > 0 && *s1 && *s2) {
     if (*s1 != *s2) {
-      return (int)((unsigned char)(*s1) - (unsigned char)(*s2));
+      return *(unsigned char *)s1 - *(unsigned char *)s2;
     }
     s1++;
     s2++;
     n--;
   }
-  return (n > 0) ? (int)((unsigned char)(*s1) - (unsigned char)(*s2)) : 0;
+  return (n > 0) ? *(unsigned char *)s1 - *(unsigned char *)s2 : 0;
 }
 
 void *memset(void *s, int c, size_t n) {
