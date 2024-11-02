09678eb4aaa04f7ed83fbf4b6dd9c438b0a911b6
tracer-ics2024 <tracer@njuics.org>
2024-11-02 14:27:00 +0800

    >  compile NEMU 231250160 左涛 Linux zeet-XiaoXinPro-16-APH8 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:27:00 up  4:11,  1 user,  load average: 0.73, 0.74, 0.65

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -64,19 +64,53 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+  unsigned char *p = s;
+  while (n--) {
+    *p++ = (unsigned char)c;
+  }
+  return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
-  panic("Not implemented");
+  unsigned char *d = dst;
+  const unsigned char *s = src;
+  if (d < s) {
+    while (n--)
+    {
+      *d++ = *s++;
+    }
+  } else {
+    d += n;
+    s += n;
+    while (n--) {
+      *(--d) = *(--s);
+    }
+  }
+  return dst;
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
-  panic("Not implemented");
+  unsigned char *d = out;
+  const unsigned char *s = in;
+  while (n--)
+  {
+    *d++ = *s++;
+  }
+  return out;
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+  const unsigned char *p1 = s1;
+  const unsigned char *p2 = s2;
+  while (n--)
+  {
+    if (*p1 != *p2) {
+      return *p1 - *p2;
+    }
+    p1++;
+    p2++;
+  }
+  return 0;
 }
 
 #endif
