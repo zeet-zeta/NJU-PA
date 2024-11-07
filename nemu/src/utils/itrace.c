0cf9acbba76fee4357ecb7f2dd3f064333c256e9
tracer-ics2024 <tracer@njuics.org>
2024-11-07 16:05:14 +0800

    >  compile NEMU 231250160 左涛 Linux zeet-XiaoXinPro-16-APH8 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:05:14 up  2:03,  1 user,  load average: 0.60, 0.74, 0.71

---
+++ nemu/src/utils/itrace.c
@@ -0,0 +1,35 @@
+#include <common.h>
+#define MAX_RING_BUF 5
+
+typedef struct {
+    uint32_t pc;
+    uint32_t inst;
+} ItraceNode;
+
+ItraceNode iringbuf[MAX_RING_BUF];
+int cur = 0;
+bool full = false;
+
+void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
+
+void itrace_add(uint32_t pc, uint32_t inst) {
+    iringbuf[cur].pc = pc;
+    iringbuf[cur].inst = inst;
+    cur = (cur + 1) % MAX_RING_BUF;
+    full = full || cur == 0;
+}
+
+void itrace_display() {
+    if (!cur && !full) return;
+    int i = full ? cur : 0;
+    int end = cur;
+    printf("===== the nearest %d instructions =====", MAX_RING_BUF);
+    char longbuf[128];
+    do {
+        char *p = longbuf;
+        p += snprintf(p, sizeof(longbuf), FMT_WORD ": %08x", iringbuf[i].pc, iringbuf[i].inst);
+        disassemble(p, longbuf + sizeof(longbuf) - p, iringbuf[i].pc, (uint8_t *)&iringbuf[i].inst, 4);
+        puts(longbuf);
+    } while ((i = (i + 1) % MAX_RING_BUF) != end);
+    printf("=======================================");
+}
