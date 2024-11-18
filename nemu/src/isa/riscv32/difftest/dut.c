26fee5eac15557e6294ba6b9172ff717e4b1f4d3
tracer-ics2024 <tracer@njuics.org>
2024-11-18 13:54:01 +0800

    >  compile NEMU 231250160 左涛 Linux zeet-XiaoXinPro-16-APH8 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:54:01 up  3:48,  1 user,  load average: 0.56, 0.82, 0.81

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -18,7 +18,7 @@
 #include "../local-include/reg.h"
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   for (int i = 0; i < 32; i++) {
-    if (ref_r->gpr[i] != cpu.gpr[i]) {
+    if (ref_r->gpr[i] != cpu.gpr[i + 1]) {
       pc = cpu.pc;
       return false;
     }
