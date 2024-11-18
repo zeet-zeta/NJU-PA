882b36d9cef94143bafd7935f14bd8495bbd1b6d
tracer-ics2024 <tracer@njuics.org>
2024-11-18 22:57:08 +0800

    >  compile NEMU 231250160 左涛 Linux zeet-XiaoXinPro-16-APH8 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:57:08 up  1:12,  1 user,  load average: 0.68, 0.56, 0.49

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -90,8 +90,8 @@ void *memmove(void *dst, const void *src, size_t n) {
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
-  char *d = (char *)out;
-  char *s = (char *)in;
+  unsigned char *d = (unsigned char *)out;
+  unsigned char *s = (unsigned char *)in;
   for (size_t i = 0; i < n; i++) {
     d[i] = s[i];
   }
