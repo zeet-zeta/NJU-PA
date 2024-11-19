2f67d1b56d611e08d4c031f1c5d26de3662c8a94
tracer-ics2024 <tracer@njuics.org>
2024-11-19 19:04:50 +0800

    >  compile NEMU 231250160 左涛 Linux zeet-XiaoXinPro-16-APH8 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:04:50 up  9:51,  1 user,  load average: 0.47, 0.75, 0.74

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -5,6 +5,6 @@
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   uint32_t temp = inl(KBD_ADDR);
-  kbd->keydown = temp == KEYDOWN_MASK ? false : true;
+  kbd->keydown = temp & KEYDOWN_MASK ? true : false;
   kbd->keycode = temp & ~KEYDOWN_MASK;
 }
