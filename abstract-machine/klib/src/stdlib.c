f23e25abf4ac55a8bb83d46a0239baaa0b615d72
tracer-ics2024 <tracer@njuics.org>
2024-11-19 20:18:50 +0800

    >  compile NEMU 231250160 左涛 Linux zeet-XiaoXinPro-16-APH8 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:18:50 up 11:05,  1 user,  load average: 1.55, 0.95, 0.75

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -1,58 +1,58 @@
-#include <am.h>
-#include <klib.h>
-#include <klib-macros.h>
+// #include <am.h>
+// #include <klib.h>
+// #include <klib-macros.h>
 
-#if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
-static unsigned long int next = 1;
+// #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
+// static unsigned long int next = 1;
 
-int rand(void) {
-  // RAND_MAX assumed to be 32767
-  next = next * 1103515245 + 12345;
-  return (unsigned int)(next/65536) % 32768;
-}
+// int rand(void) {
+//   // RAND_MAX assumed to be 32767
+//   next = next * 1103515245 + 12345;
+//   return (unsigned int)(next/65536) % 32768;
+// }
 
-void srand(unsigned int seed) {
-  next = seed;
-}
+// void srand(unsigned int seed) {
+//   next = seed;
+// }
 
-int abs(int x) {
-  return (x < 0 ? -x : x);
-}
+// int abs(int x) {
+//   return (x < 0 ? -x : x);
+// }
 
-int atoi(const char* nptr) {
-  int x = 0;
-  while (*nptr == ' ') { nptr ++; }
-  while (*nptr >= '0' && *nptr <= '9') {
-    x = x * 10 + *nptr - '0';
-    nptr ++;
-  }
-  return x;
-}
+// int atoi(const char* nptr) {
+//   int x = 0;
+//   while (*nptr == ' ') { nptr ++; }
+//   while (*nptr >= '0' && *nptr <= '9') {
+//     x = x * 10 + *nptr - '0';
+//     nptr ++;
+//   }
+//   return x;
+// }
 
-void *malloc(size_t size) {
-  // On native, malloc() will be called during initializaion of C runtime.
-  // Therefore do not call panic() here, else it will yield a dead recursion:
-  //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
-#if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  static void *addr = NULL;
-  if (addr == NULL) {
-    addr = heap.start;
-  }
-  if (addr + size > heap.end) {
-    return NULL;
-  }
-  void *ptr = addr + size;
-  uintptr_t aligned_ptr = (uintptr_t)ptr;
-  if (aligned_ptr % 8 != 0) {
-    aligned_ptr += (8 - (aligned_ptr % 8));
-  }
-  return (void *)aligned_ptr;
+// void *malloc(size_t size) {
+//   // On native, malloc() will be called during initializaion of C runtime.
+//   // Therefore do not call panic() here, else it will yield a dead recursion:
+//   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
+// #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
+//   static void *addr = NULL;
+//   if (addr == NULL) {
+//     addr = heap.start;
+//   }
+//   if (addr + size > heap.end) {
+//     return NULL;
+//   }
+//   void *ptr = addr + size;
+//   uintptr_t aligned_ptr = (uintptr_t)ptr;
+//   if (aligned_ptr % 8 != 0) {
+//     aligned_ptr += (8 - (aligned_ptr % 8));
+//   }
+//   return (void *)aligned_ptr;
   
-#endif
-  return NULL;
-}
+// #endif
+//   return NULL;
+// }
 
-void free(void *ptr) {
-}
+// void free(void *ptr) {
+// }
 
-#endif
+// #endif
--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -1,115 +1,115 @@
-#include <klib.h>
-#include <klib-macros.h>
-#include <stdint.h>
+// #include <klib.h>
+// #include <klib-macros.h>
+// #include <stdint.h>
 
-#if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
+// #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-size_t strlen(const char *s) {
-  const char *t = s;
-  while (*t) t++;
-  return t - s;
-}
+// size_t strlen(const char *s) {
+//   const char *t = s;
+//   while (*t) t++;
+//   return t - s;
+// }
 
-char *strcpy(char *dst, const char *src) {
-  char *original_dst = dst;
-  while (*src) {
-    *dst++ = *src++;
-  }
-  *dst = '\0';
-  return original_dst;
-}
+// char *strcpy(char *dst, const char *src) {
+//   char *original_dst = dst;
+//   while (*src) {
+//     *dst++ = *src++;
+//   }
+//   *dst = '\0';
+//   return original_dst;
+// }
 
-char *strncpy(char *dst, const char *src, size_t n) {
-  char *original_dst = dst;
-  while (n > 0 && *src) {
-    *dst++ = *src++;
-    n--;
-  }
-  while (n > 0)
-  {
-    *dst++ = '\0';
-    n--;
-  }
-  return original_dst;
-}
+// char *strncpy(char *dst, const char *src, size_t n) {
+//   char *original_dst = dst;
+//   while (n > 0 && *src) {
+//     *dst++ = *src++;
+//     n--;
+//   }
+//   while (n > 0)
+//   {
+//     *dst++ = '\0';
+//     n--;
+//   }
+//   return original_dst;
+// }
 
-char *strcat(char *dst, const char *src) {
-  char *original_dst = dst;
-  while (*dst) dst++;
-  while (*src) {
-    *dst++ = *src++;
-  }
-  *dst = '\0';
-  return original_dst;
-}
+// char *strcat(char *dst, const char *src) {
+//   char *original_dst = dst;
+//   while (*dst) dst++;
+//   while (*src) {
+//     *dst++ = *src++;
+//   }
+//   *dst = '\0';
+//   return original_dst;
+// }
 
-int strcmp(const char *s1, const char *s2) {
-  while (*s1 && (*s1 == *s2)) {
-    s1++;
-    s2++;
-  }
-  return *(unsigned char *)s1 - *(unsigned char *)s2;
-}
+// int strcmp(const char *s1, const char *s2) {
+//   while (*s1 && (*s1 == *s2)) {
+//     s1++;
+//     s2++;
+//   }
+//   return *(unsigned char *)s1 - *(unsigned char *)s2;
+// }
 
-int strncmp(const char *s1, const char *s2, size_t n) {
-  while (n > 0 && *s1 && *s2) {
-    if (*s1 != *s2) {
-      return *(unsigned char *)s1 - *(unsigned char *)s2;
-    }
-    s1++;
-    s2++;
-    n--;
-  }
-  return (n > 0) ? *(unsigned char *)s1 - *(unsigned char *)s2 : 0;
-}
+// int strncmp(const char *s1, const char *s2, size_t n) {
+//   while (n > 0 && *s1 && *s2) {
+//     if (*s1 != *s2) {
+//       return *(unsigned char *)s1 - *(unsigned char *)s2;
+//     }
+//     s1++;
+//     s2++;
+//     n--;
+//   }
+//   return (n > 0) ? *(unsigned char *)s1 - *(unsigned char *)s2 : 0;
+// }
 
-void *memset(void *s, int c, size_t n) {
-  unsigned char *p = s;
-  while (n--) {
-    *p++ = (unsigned char)c;
-  }
-  return s;
-}
+// void *memset(void *s, int c, size_t n) {
+//   unsigned char *p = s;
+//   while (n--) {
+//     *p++ = (unsigned char)c;
+//   }
+//   return s;
+// }
 
-void *memmove(void *dst, const void *src, size_t n) {
-  unsigned char *d = dst;
-  const unsigned char *s = src;
-  if (d < s) {
-    while (n--)
-    {
-      *d++ = *s++;
-    }
-  } else {
-    d += n;
-    s += n;
-    while (n--) {
-      *(--d) = *(--s);
-    }
-  }
-  return dst;
-}
+// void *memmove(void *dst, const void *src, size_t n) {
+//   unsigned char *d = dst;
+//   const unsigned char *s = src;
+//   if (d < s) {
+//     while (n--)
+//     {
+//       *d++ = *s++;
+//     }
+//   } else {
+//     d += n;
+//     s += n;
+//     while (n--) {
+//       *(--d) = *(--s);
+//     }
+//   }
+//   return dst;
+// }
 
-void *memcpy(void *out, const void *in, size_t n) {
-    char* d = (char*) out;
-    const char* s = (const char*) in;
-    while (n--)
-      *d++ = *s++;
-    return out;
-}
+// void *memcpy(void *out, const void *in, size_t n) {
+//     char* d = (char*) out;
+//     const char* s = (const char*) in;
+//     while (n--)
+//       *d++ = *s++;
+//     return out;
+// }
 
-int memcmp(const void *s1, const void *s2, size_t n) {
-  const unsigned char *p1 = s1;
-  const unsigned char *p2 = s2;
-  while (n--)
-  {
-    if (*p1 != *p2) {
-      return *p1 - *p2;
-    }
-    p1++;
-    p2++;
-  }
-  return 0;
-}
+// int memcmp(const void *s1, const void *s2, size_t n) {
+//   const unsigned char *p1 = s1;
+//   const unsigned char *p2 = s2;
+//   while (n--)
+//   {
+//     if (*p1 != *p2) {
+//       return *p1 - *p2;
+//     }
+//     p1++;
+//     p2++;
+//   }
+//   return 0;
+// }
 
-#endif
-//完工
+// #endif
+// //完工
