f0ee3e9b25023139b578d22904466cd8954be047
tracer-ics2024 <tracer@njuics.org>
2024-11-19 09:37:16 +0800

    >  compile NEMU 231250160 左涛 Linux zeet-XiaoXinPro-16-APH8 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:37:16 up 23 min,  1 user,  load average: 0.73, 1.01, 0.71

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -44,12 +44,11 @@ char *strcat(char *dst, const char *src) {
 }
 
 int strcmp(const char *s1, const char *s2) {
-  // while (*s1 && (*s1 == *s2)) {
-  //   s1++;
-  //   s2++;
-  // }
-  // return *(unsigned char *)s1 - *(unsigned char *)s2;
-  return 0;
+  while (*s1 && (*s1 == *s2)) {
+    s1++;
+    s2++;
+  }
+  return *(unsigned char *)s1 - *(unsigned char *)s2;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
