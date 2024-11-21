1c71c187abe4f94b1b1a28b2b4110fbb20306f0c
tracer-ics2024 <tracer@njuics.org>
2024-11-21 15:39:50 +0800

    >  compile NEMU 231250160 左涛 Linux zeet-XiaoXinPro-16-APH8 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:39:50 up  6:13,  1 user,  load average: 1.38, 1.16, 1.04

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -127,70 +127,70 @@ static int decode_exec(Decode *s) {
           }  
       }
 
-      case 0x17: //U
-        decode_operand(s, &rd, &src1, &src2, &imm, TYPE_U);
-        R(rd) = s->pc + imm; BREAK;
-      case 0x37: //U
-        decode_operand(s, &rd, &src1, &src2, &imm, TYPE_U);
-        R(rd) = imm; BREAK;
+    case 0x17: //U
+      decode_operand(s, &rd, &src1, &src2, &imm, TYPE_U);
+      R(rd) = s->pc + imm; BREAK;
+    case 0x37: //U
+      decode_operand(s, &rd, &src1, &src2, &imm, TYPE_U);
+      R(rd) = imm; BREAK;
 
-      case 0x13: //I
-        decode_operand(s, &rd, &src1, &src2, &imm, TYPE_I);
-        switch (FUNC3) {
-          case 0x0: R(rd) = src1 + imm; BREAK;
-          case 0x1000: 
-            switch (FUNC7) {
-              case 0x0: R(rd) = src1 << imm; BREAK;
-            }
-          case 0x2000: R(rd) = (int32_t) src1 < (int32_t) imm; BREAK;
-          case 0x3000: R(rd) = src1 < imm; BREAK;
-          case 0x4000: R(rd) = src1 ^ imm; BREAK;
-          case 0x5000: 
-            switch (FUNC7) {
-              case 0x0: R(rd) = src1 >> imm; BREAK;
-              case 0x40000000: R(rd) = (int32_t) src1 >> (imm & 0x1f); BREAK;
-            }
-          case 0x6000: R(rd) = src1 | imm; BREAK;
-          case 0x7000: R(rd) = src1 & imm; BREAK;
-        }
-      case 0x3: //I
-        decode_operand(s, &rd, &src1, &src2, &imm, TYPE_I);
-        switch (FUNC3) {
-          case 0x0: R(rd) = (word_t) ((int32_t) Mr(src1 + imm, 1)) << 24 >> 24; BREAK;
-          case 0x1000: R(rd) = (word_t) ((int32_t) Mr(src1 + imm, 2) << 16 >> 16); BREAK;
-          case 0x2000: R(rd) = Mr(src1 + imm, 4); BREAK;
-          case 0x4000: R(rd) = Mr(src1 + imm, 1); BREAK;
-          case 0x5000: R(rd) = Mr(src1 + imm, 2); BREAK;
-        }
-      case 0x67: //I
-        decode_operand(s, &rd, &src1, &src2, &imm, TYPE_I);
-        switch (FUNC3) {
-          case 0x0: R(rd) = s->snpc, s-> dnpc = src1 + imm; IFDEF(CONFIG_FTRACE, ftrace_jalr(rd, s->pc, s->dnpc, s->isa.inst.val)); BREAK;
-        }
-      case 0x23: //S
-        decode_operand(s, &rd, &src1, &src2, &imm, TYPE_S);
-        switch (FUNC3) {
-          case 0x0: Mw(src1 + imm, 1, src2); BREAK;
-          case 0x1000: Mw(src1 + imm, 2, src2); BREAK;
-          case 0x2000: Mw(src1 + imm, 4, src2); BREAK;
-        }
-      case 0x63: //B
-        decode_operand(s, &rd, &src1, &src2, &imm, TYPE_B);
-        switch (FUNC3) {
-          case 0x0: s->dnpc = src1 == src2 ? s->pc + imm : s->dnpc; BREAK;
-          case 0x1000: s->dnpc = src1 == src2 ? s->dnpc : s->pc + imm; BREAK;
-          case 0x4000: s->dnpc = (int32_t) src1 < (int32_t) src2 ? s->pc + imm : s->dnpc; BREAK;
-          case 0x5000: s->dnpc = (int32_t) src1 >= (int32_t) src2 ? s->pc + imm : s->dnpc; BREAK;
-          case 0x6000: s->dnpc = src1 < src2 ? s->pc + imm : s->dnpc; BREAK;
-          case 0x7000: s->dnpc = src1 >= src2 ? s->pc + imm : s->dnpc; BREAK;
-        }
-      case 0x6f:
-        decode_operand(s, &rd, &src1, &src2, &imm, TYPE_J);
-        R(rd) = s->snpc, s->dnpc = s->pc + imm; IFDEF(CONFIG_FTRACE, ftrace_jal(rd, s->pc, s->dnpc)); BREAK;
-      case 0x73:
-        NEMUTRAP(s->pc, R(10)); BREAK;
-      Assert(0, "INVALID INST");
-    }
+    case 0x13: //I
+      decode_operand(s, &rd, &src1, &src2, &imm, TYPE_I);
+      switch (FUNC3) {
+        case 0x0: R(rd) = src1 + imm; BREAK;
+        case 0x1000: 
+          switch (FUNC7) {
+            case 0x0: R(rd) = src1 << imm; BREAK;
+          }
+        case 0x2000: R(rd) = (int32_t) src1 < (int32_t) imm; BREAK;
+        case 0x3000: R(rd) = src1 < imm; BREAK;
+        case 0x4000: R(rd) = src1 ^ imm; BREAK;
+        case 0x5000: 
+          switch (FUNC7) {
+            case 0x0: R(rd) = src1 >> imm; BREAK;
+            case 0x40000000: R(rd) = (int32_t) src1 >> (imm & 0x1f); BREAK;
+          }
+        case 0x6000: R(rd) = src1 | imm; BREAK;
+        case 0x7000: R(rd) = src1 & imm; BREAK;
+      }
+    case 0x3: //I
+      decode_operand(s, &rd, &src1, &src2, &imm, TYPE_I);
+      switch (FUNC3) {
+        case 0x0: R(rd) = (word_t) ((int32_t) Mr(src1 + imm, 1)) << 24 >> 24; BREAK;
+        case 0x1000: R(rd) = (word_t) ((int32_t) Mr(src1 + imm, 2) << 16 >> 16); BREAK;
+        case 0x2000: R(rd) = Mr(src1 + imm, 4); BREAK;
+        case 0x4000: R(rd) = Mr(src1 + imm, 1); BREAK;
+        case 0x5000: R(rd) = Mr(src1 + imm, 2); BREAK;
+      }
+    case 0x67: //I
+      decode_operand(s, &rd, &src1, &src2, &imm, TYPE_I);
+      switch (FUNC3) {
+        case 0x0: R(rd) = s->snpc, s-> dnpc = src1 + imm; IFDEF(CONFIG_FTRACE, ftrace_jalr(rd, s->pc, s->dnpc, s->isa.inst.val)); BREAK;
+      }
+    case 0x23: //S
+      decode_operand(s, &rd, &src1, &src2, &imm, TYPE_S);
+      switch (FUNC3) {
+        case 0x0: Mw(src1 + imm, 1, src2); BREAK;
+        case 0x1000: Mw(src1 + imm, 2, src2); BREAK;
+        case 0x2000: Mw(src1 + imm, 4, src2); BREAK;
+      }
+    case 0x63: //B
+      decode_operand(s, &rd, &src1, &src2, &imm, TYPE_B);
+      switch (FUNC3) {
+        case 0x0: s->dnpc = src1 == src2 ? s->pc + imm : s->dnpc; BREAK;
+        case 0x1000: s->dnpc = src1 == src2 ? s->dnpc : s->pc + imm; BREAK;
+        case 0x4000: s->dnpc = (int32_t) src1 < (int32_t) src2 ? s->pc + imm : s->dnpc; BREAK;
+        case 0x5000: s->dnpc = (int32_t) src1 >= (int32_t) src2 ? s->pc + imm : s->dnpc; BREAK;
+        case 0x6000: s->dnpc = src1 < src2 ? s->pc + imm : s->dnpc; BREAK;
+        case 0x7000: s->dnpc = src1 >= src2 ? s->pc + imm : s->dnpc; BREAK;
+      }
+    case 0x6f: //J
+      decode_operand(s, &rd, &src1, &src2, &imm, TYPE_J);
+      R(rd) = s->snpc, s->dnpc = s->pc + imm; IFDEF(CONFIG_FTRACE, ftrace_jal(rd, s->pc, s->dnpc)); BREAK;
+    case 0x73: //ebreak
+      NEMUTRAP(s->pc, R(10)); BREAK;
+    Assert(0, "INVALID INST");
+  }
 
 
   INSTPAT_END();
