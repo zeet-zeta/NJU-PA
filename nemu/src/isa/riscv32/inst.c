3cd5b9c3f3ef326c541c58f2a0c6f76b4fcd49df
tracer-ics2024 <tracer@njuics.org>
2024-11-21 16:23:21 +0800

    >  compile NEMU 231250160 左涛 Linux zeet-XiaoXinPro-16-APH8 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:23:21 up  6:57,  1 user,  load average: 1.06, 0.88, 0.88

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -65,22 +65,21 @@ static int decode_exec(Decode *s) {
 //   __VA_ARGS__ ; 
 // } // __VA_ARGS__是一个特殊的宏，可作为语句执行
 
-#define OPCODE (i & 0x7f)
-#define FUNC3 (i & 0x7000)
-#define FUNC7 (i & 0xfe000000)
 #define BREAK goto *(__instpat_end)
-
 
   INSTPAT_START();
 
   uint32_t i = s->isa.inst.val;
+  uint32_t OPCODE = i & 0x7f;
+  uint32_t FUNC3 = i & 0x7000;
+  uint32_t FUNC7 = i & 0xfe000000;
+
   int rs1 = BITS(i, 19, 15); //寄存器1
   int rs2 = BITS(i, 24, 20); //寄存器2
   rd = BITS(i, 11, 7); //目标寄存器 
-  src1R(); src2R();
-  // printf("%x\n", cur_inst);
   switch (OPCODE) {
     case 0x33: // R
+      src1R(); src2R();
       switch (FUNC3) {
         case 0x0: 
           switch (FUNC7) {
@@ -138,7 +137,7 @@ static int decode_exec(Decode *s) {
       R(rd) = imm; BREAK;
 
     case 0x13: //I
-      immI();
+      immI(); src1R();
       switch (FUNC3) {
         case 0x0: R(rd) = src1 + imm; BREAK;
         case 0x1000: 
@@ -157,7 +156,7 @@ static int decode_exec(Decode *s) {
         case 0x7000: R(rd) = src1 & imm; BREAK;
       }
     case 0x3: //I
-      immI();
+      immI(); src1R();
       switch (FUNC3) {
         case 0x0: R(rd) = (word_t) ((int32_t) Mr(src1 + imm, 1) << 24 >> 24); BREAK;
         case 0x1000: R(rd) = (word_t) ((int32_t) Mr(src1 + imm, 2) << 16 >> 16); BREAK;
@@ -166,19 +165,19 @@ static int decode_exec(Decode *s) {
         case 0x5000: R(rd) = Mr(src1 + imm, 2); BREAK;
       }
     case 0x67: //I
-      immI();
+      immI(); src1R();
       switch (FUNC3) {
         case 0x0: R(rd) = s->snpc, s-> dnpc = src1 + imm; IFDEF(CONFIG_FTRACE, ftrace_jalr(rd, s->pc, s->dnpc, s->isa.inst.val)); BREAK;
       }
     case 0x23: //S
-      immS();
+      immS(); src1R(); src2R();
       switch (FUNC3) {
         case 0x0: Mw(src1 + imm, 1, src2); BREAK;
         case 0x1000: Mw(src1 + imm, 2, src2); BREAK;
         case 0x2000: Mw(src1 + imm, 4, src2); BREAK;
       }
     case 0x63: //B
-      immB();
+      immB(); src1R(); src2R();
       switch (FUNC3) {
         case 0x0: s->dnpc = src1 == src2 ? s->pc + imm : s->dnpc; BREAK;
         case 0x1000: s->dnpc = src1 == src2 ? s->dnpc : s->pc + imm; BREAK;
