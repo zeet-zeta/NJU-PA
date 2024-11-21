268fcc396bec2af9e3b2c45f184f6ddc017006f1
tracer-ics2024 <tracer@njuics.org>
2024-11-21 12:52:33 +0800

    >  compile NEMU 231250160 左涛 Linux zeet-XiaoXinPro-16-APH8 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:52:33 up  3:26,  1 user,  load average: 1.01, 0.60, 0.40

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -26,27 +26,27 @@
 #define MAX_INST_TO_PRINT 10
 
 CPU_state cpu = {};
-uint64_t g_nr_guest_inst = 0;
-static uint64_t g_timer = 0; // unit: us
-static bool g_print_step = false;
+// uint64_t g_nr_guest_inst = 0;
+// static uint64_t g_timer = 0; // unit: us
+// static bool g_print_step = false;
 
 void device_update();
 
-static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
-#ifdef CONFIG_ITRACE_COND
-  if (ITRACE_COND) { log_write("%s\n", _this->logbuf); }
-#endif
-  if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
-  IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
+// static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
+// #ifdef CONFIG_ITRACE_COND
+//   if (ITRACE_COND) { log_write("%s\n", _this->logbuf); }
+// #endif
+//   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
+//   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
 
-  //监视点，删去可以提高性能
-  // bool has_change = false;
-  // scan_all(&has_change);
-  // if (has_change) {
-  //   nemu_state.state = NEMU_STOP;
-  //   Log("trigger watchpoint");
-  // }
-}
+//   //监视点，删去可以提高性能
+//   // bool has_change = false;
+//   // scan_all(&has_change);
+//   // if (has_change) {
+//   //   nemu_state.state = NEMU_STOP;
+//   //   Log("trigger watchpoint");
+//   // }
+// }
 
 static void exec_once(Decode *s, vaddr_t pc) {
   s->pc = pc;
@@ -82,8 +82,8 @@ static void execute(uint64_t n) {
   Decode s;
   for (;n > 0; n --) {
     exec_once(&s, cpu.pc);
-    g_nr_guest_inst ++;
-    trace_and_difftest(&s, cpu.pc);
+    // g_nr_guest_inst ++;
+    // trace_and_difftest(&s, cpu.pc);
     if (nemu_state.state != NEMU_RUNNING) break;
     IFDEF(CONFIG_DEVICE, device_update());
   }
@@ -96,11 +96,11 @@ static void statistic() {
   // void itrace_display();
   // itrace_display();
   isa_reg_display();
-#define NUMBERIC_FMT MUXDEF(CONFIG_TARGET_AM, "%", "%'") PRIu64
-  Log("host time spent = " NUMBERIC_FMT " us", g_timer);
-  Log("total guest instructions = " NUMBERIC_FMT, g_nr_guest_inst);
-  if (g_timer > 0) Log("simulation frequency = " NUMBERIC_FMT " inst/s", g_nr_guest_inst * 1000000 / g_timer);
-  else Log("Finish running in less than 1 us and can not calculate the simulation frequency");
+// #define NUMBERIC_FMT MUXDEF(CONFIG_TARGET_AM, "%", "%'") PRIu64
+//   Log("host time spent = " NUMBERIC_FMT " us", g_timer);
+//   Log("total guest instructions = " NUMBERIC_FMT, g_nr_guest_inst);
+//   if (g_timer > 0) Log("simulation frequency = " NUMBERIC_FMT " inst/s", g_nr_guest_inst * 1000000 / g_timer);
+//   else Log("Finish running in less than 1 us and can not calculate the simulation frequency");
 }
 
 void assert_fail_msg() {
@@ -109,7 +109,7 @@ void assert_fail_msg() {
 
 /* Simulate how the CPU works. */
 void cpu_exec(uint64_t n) {
-  g_print_step = (n < MAX_INST_TO_PRINT);
+  // g_print_step = (n < MAX_INST_TO_PRINT);
   switch (nemu_state.state) {
     case NEMU_END: case NEMU_ABORT: case NEMU_QUIT:
       printf("Program execution has ended. To restart the program, exit NEMU and run again.\n");
@@ -117,12 +117,12 @@ void cpu_exec(uint64_t n) {
     default: nemu_state.state = NEMU_RUNNING;
   }
 
-  uint64_t timer_start = get_time();
+  // uint64_t timer_start = get_time();
 
   execute(n);
 
-  uint64_t timer_end = get_time();
-  g_timer += timer_end - timer_start;
+  // uint64_t timer_end = get_time();
+  // g_timer += timer_end - timer_start;
 
   switch (nemu_state.state) {
     case NEMU_RUNNING: nemu_state.state = NEMU_STOP; break;
