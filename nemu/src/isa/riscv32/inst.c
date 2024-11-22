375e2f88bec2d27ba6eab507e4a737b0b7e0cf58
tracer-ics2024 <tracer@njuics.org>
2024-11-22 08:35:36 +0800

    >  compile NEMU 231250160 左涛 Linux zeet-XiaoXinPro-16-APH8 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  08:35:36 up 5 min,  1 user,  load average: 0.94, 0.59, 0.28

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -79,7 +79,7 @@ static int decode_exec(Decode *s) {
 
 #define BREAK R(0) = 0; return 0
 #define OPCODE (i & 0x7f)
-#define FUNC3 ((i >> 12) & 0x7)
+#define FUNC3 (i & 0x7)
 #define FUNC7 ((i >> 25) & 0xef)
   uint32_t i = s->isa.inst.val;
   // switch (OPCODE) {
@@ -88,11 +88,11 @@ static int decode_exec(Decode *s) {
       immI(); src1R(); RD();
       switch (FUNC3) {
         case 0x0: R(rd) = src1 + imm; BREAK;
-        case 0x1: R(rd) = src1 << imm; BREAK;
-        case 0x2: R(rd) = (int32_t) src1 < (int32_t) imm; BREAK;
-        case 0x3: R(rd) = src1 < imm; BREAK;
-        case 0x4: R(rd) = src1 ^ imm; BREAK;
-        case 0x5: 
+        case 0x1000: R(rd) = src1 << imm; BREAK;
+        case 0x2000: R(rd) = (int32_t) src1 < (int32_t) imm; BREAK;
+        case 0x3000: R(rd) = src1 < imm; BREAK;
+        case 0x4000: R(rd) = src1 ^ imm; BREAK;
+        case 0x5000: 
           switch (FUNC7) {
             case 0x0: R(rd) = src1 >> imm; BREAK;
             case 0x20: R(rd) = (int32_t) src1 >> (imm & 0x1f); BREAK;
@@ -107,17 +107,17 @@ static int decode_exec(Decode *s) {
       immI(); src1R(); RD();
       switch (FUNC3) {
         case 0x0: R(rd) = (word_t) ((int32_t) Mr(src1 + imm, 1) << 24 >> 24); BREAK;
-        case 0x1: R(rd) = (word_t) ((int32_t) Mr(src1 + imm, 2) << 16 >> 16); BREAK;
-        case 0x2: R(rd) = Mr(src1 + imm, 4); BREAK;
-        case 0x4: R(rd) = Mr(src1 + imm, 1); BREAK;
-        case 0x5: R(rd) = Mr(src1 + imm, 2); BREAK;
+        case 0x1000: R(rd) = (word_t) ((int32_t) Mr(src1 + imm, 2) << 16 >> 16); BREAK;
+        case 0x2000: R(rd) = Mr(src1 + imm, 4); BREAK;
+        case 0x4000: R(rd) = Mr(src1 + imm, 1); BREAK;
+        case 0x5000: R(rd) = Mr(src1 + imm, 2); BREAK;
       }
     } else if (opcode == 0x23) {
       immS(); src1R(); src2R();
       switch (FUNC3) {
         case 0x0: Mw(src1 + imm, 1, src2); BREAK;
-        case 0x1: Mw(src1 + imm, 2, src2); BREAK;
-        case 0x2: Mw(src1 + imm, 4, src2); BREAK;
+        case 0x1000: Mw(src1 + imm, 2, src2); BREAK;
+        case 0x2000: Mw(src1 + imm, 4, src2); BREAK;
       }
     } else if (opcode == 0x37) {
       immU(); RD();
@@ -134,42 +134,42 @@ static int decode_exec(Decode *s) {
             case 0x20: R(rd) = src1 - src2; BREAK;
             case 0x1: R(rd) = src1 * src2; BREAK;
           }
-        case 0x1: 
+        case 0x1000: 
           switch (FUNC7) {
             case 0x0: R(rd) = src1 << (src2 & 0x1f); BREAK;
             case 0x1: R(rd) = (1ll * (int32_t) src1 * (int32_t) src2) >> 32; BREAK;
           }
-        case 0x2: 
+        case 0x2000: 
           switch (FUNC7) {
             case 0x0: R(rd) = (int32_t) src1 < (int32_t) src2; BREAK;
             case 0x1: R(rd) = (1ll * (int32_t) src1 * (uint32_t) src2) >> 32; BREAK;
           }
         
-        case 0x3: 
+        case 0x3000: 
           switch (FUNC7) {
             case 0x0: R(rd) = src1 < src2; BREAK;
             case 0x1: R(rd) = (1ull * (uint32_t) src1 * (uint32_t) src2) >> 32; BREAK;
           }
         
-        case 0x4: 
+        case 0x4000: 
           switch (FUNC7) {
             case 0x0: R(rd) = src1 ^ src2; BREAK;
             case 0x1: R(rd) = (int32_t) src1 / (int32_t) src2; BREAK;
           }
         
-        case 0x5: 
+        case 0x5000: 
           switch (FUNC7) {
             case 0x0: R(rd) = src1 >> (src2 & 0x1f); BREAK;
             case 0x20: R(rd) = (int32_t) src1 >> (src2 & 0x1f); BREAK;
             case 0x1: R(rd) = src1 / src2; BREAK;
           }
-        case 0x6: 
+        case 0x6000: 
           switch (FUNC7) {
             case 0x0: R(rd) = src1 | src2; BREAK;
             case 0x1: R(rd) = (int32_t) src1 % (int32_t) src2; BREAK; 
           }
         
-        case 0x7: 
+        case 0x7000: 
           switch (FUNC7) {
             case 0x0: R(rd) = src1 & src2; BREAK;
             case 0x1: R(rd) = src1 % src2; BREAK;
@@ -179,11 +179,11 @@ static int decode_exec(Decode *s) {
       immB(); src1R(); src2R();
       switch (FUNC3) {
         case 0x0: s->dnpc = src1 == src2 ? s->pc + imm : s->dnpc; BREAK;
-        case 0x1: s->dnpc = src1 == src2 ? s->dnpc : s->pc + imm; BREAK;
-        case 0x4: s->dnpc = (int32_t) src1 < (int32_t) src2 ? s->pc + imm : s->dnpc; BREAK;
-        case 0x5: s->dnpc = (int32_t) src1 >= (int32_t) src2 ? s->pc + imm : s->dnpc; BREAK;
-        case 0x6: s->dnpc = src1 < src2 ? s->pc + imm : s->dnpc; BREAK;
-        case 0x7: s->dnpc = src1 >= src2 ? s->pc + imm : s->dnpc; BREAK;
+        case 0x1000: s->dnpc = src1 == src2 ? s->dnpc : s->pc + imm; BREAK;
+        case 0x4000: s->dnpc = (int32_t) src1 < (int32_t) src2 ? s->pc + imm : s->dnpc; BREAK;
+        case 0x5000: s->dnpc = (int32_t) src1 >= (int32_t) src2 ? s->pc + imm : s->dnpc; BREAK;
+        case 0x6000: s->dnpc = src1 < src2 ? s->pc + imm : s->dnpc; BREAK;
+        case 0x7000: s->dnpc = src1 >= src2 ? s->pc + imm : s->dnpc; BREAK;
       }
     } else if (opcode == 0x67) {
       immI(); src1R(); RD();
