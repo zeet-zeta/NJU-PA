dd869df458d1910edb3e2311bcb38ad045c9c163
tracer-ics2024 <tracer@njuics.org>
2025-01-17 15:15:20 +0800

    >  compile NEMU 231250160 左涛 Linux zeet-XiaoXinPro-16-APH8 6.11.0-13-generic #14-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov 30 23:51:51 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:15:20 up  4:59,  2 users,  load average: 1.43, 1.03, 0.77

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -4,13 +4,14 @@
 
 static Context* (*user_handler)(Event, Context*) = NULL;
 
+// 这个参数来源于a0寄存器，参见trap.S
 Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
       case 0x1: 
         ev.event = EVENT_YIELD;
-        c->mepc += 4;
+        // c->mepc += 4;
         break;
       default: ev.event = EVENT_ERROR; break;
     }
@@ -26,9 +27,11 @@ extern void __am_asm_trap(void);
 
 bool cte_init(Context*(*handler)(Event, Context*)) {
   // initialize exception entry
+  // 所以在nemu中直接返回cpu.mtvec
   asm volatile("csrw mtvec, %0" : : "r"(__am_asm_trap));
 
   // register event handler
+  // 注册事件处理函数，在nanos里面调用的是cte_init(do_event);
   user_handler = handler;
 
   return true;
@@ -42,7 +45,7 @@ void yield() {
 #ifdef __riscv_e
   asm volatile("li a5, -1; ecall");
 #else
-  asm volatile("li a7, -1; ecall");
+  asm volatile("li a7, -1; ecall"); //编号
 #endif
 }
 
