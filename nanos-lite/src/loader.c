fe2e85f1024951e6687e59bc95532c7352a7c8f9
tracer-ics2024 <tracer@njuics.org>
2025-01-17 18:22:31 +0800

    >  compile NEMU 231250160 左涛 Linux zeet-XiaoXinPro-16-APH8 6.11.0-13-generic #14-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov 30 23:51:51 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:22:31 up  8:06,  2 users,  load average: 1.16, 0.97, 0.86

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -9,14 +9,32 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
+extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
+extern void ramdisk_write(const void *buf, size_t offset, size_t len);
+
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
-  return 0;
+  Elf_Ehdr ehdr;
+  Elf_Phdr phdr;
+  size_t offset = 0;
+
+  ramdisk_read(&ehdr, offset, sizeof(Elf_Ehdr));
+  offset += sizeof(Elf_Ehdr);
+
+  for (int i = 0; i < ehdr.e_phnum; i++) {
+    ramdisk_read(&phdr, ehdr.e_phoff + i * ehdr.e_phentsize, sizeof(Elf_Phdr));
+    if (phdr.p_type == PT_LOAD) {
+      ramdisk_read((void *)phdr.p_vaddr, phdr.p_offset, phdr.p_filesz);
+      if (phdr.p_filesz < phdr.p_memsz) {
+        memset((void *)(phdr.p_vaddr + phdr.p_filesz), 0, phdr.p_memsz - phdr.p_filesz);
+      }
+    }
+  }
+  return ehdr.e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
   uintptr_t entry = loader(pcb, filename);
   Log("Jump to entry = %p", entry);
-  ((void(*)())entry) ();
+  ((void(*)())entry) (); //调用刚加载的程序
 }
 
