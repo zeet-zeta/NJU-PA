e9959313dd11fadecf79f1adfcc91b0498017304
tracer-ics2024 <tracer@njuics.org>
2025-01-17 20:24:31 +0800

    >  compile NEMU 231250160 左涛 Linux zeet-XiaoXinPro-16-APH8 6.11.0-13-generic #14-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov 30 23:51:51 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:24:31 up 10:08,  2 users,  load average: 1.64, 1.24, 0.99

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,9 +1,10 @@
 #include <common.h>
 
+extern void do_syscall(Context *c);
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
     case EVENT_YIELD: Log("HIT EVENT_YIELD"); break;
-    case EVENT_SYSCALL: Log("HIT EVENT_SYSCALL"); break;
+    case EVENT_SYSCALL: do_syscall(c); break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
