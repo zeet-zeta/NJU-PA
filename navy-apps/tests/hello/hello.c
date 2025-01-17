11d1450396c1d49e2e94b84a44f9f28f46d56808
tracer-ics2024 <tracer@njuics.org>
2025-01-17 20:56:36 +0800

    >  compile NEMU 231250160 左涛 Linux zeet-XiaoXinPro-16-APH8 6.11.0-13-generic #14-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov 30 23:51:51 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:56:36 up 10:40,  2 users,  load average: 0.67, 0.73, 0.81

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -6,12 +6,12 @@ int main() {
   int i = 2;
   volatile int j = 0;
   printf("Hello World from Navy-apps !\n");
-  while (1) {
-    j ++;
-    if (j == 10000) {
-      printf("Hello World from Navy-apps for the %dth time!\n", i ++);
-      j = 0;
-    }
-  }
+  // while (1) {
+  //   j ++;
+  //   if (j == 10000) {
+  //     printf("Hello World from Navy-apps for the %dth time!\n", i ++);
+  //     j = 0;
+  //   }
+  // }
   return 0;
 }
