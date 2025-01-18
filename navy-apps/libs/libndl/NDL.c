fb1331d6934ab4a802d1a8c6779fa5b21dab9329
tracer-ics2024 <tracer@njuics.org>
2025-01-18 23:45:52 +0800

    >  compile NEMU 231250160 左涛 Linux zeet-XiaoXinPro-16-APH8 6.11.0-13-generic #14-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov 30 23:51:51 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  23:45:52 up  2:08,  2 users,  load average: 0.70, 0.61, 0.55

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -55,6 +55,7 @@ void NDL_OpenCanvas(int *w, int *h) {
 
 void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
   lseek(fbdev, (x + y * screen_w) * sizeof(uint32_t), SEEK_SET);
+  printf("DrawRect: %d %d %d %d\n", x, y, w, h);
   for (int i = 0; i < h - 1; i++) {
     write(fbdev, pixels, w * sizeof(uint32_t));
     lseek(fbdev, (screen_w - w) * sizeof(uint32_t), SEEK_CUR);
