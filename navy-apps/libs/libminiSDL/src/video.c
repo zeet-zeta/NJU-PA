3168185a7590e58c77773c814968db179d37ba96
tracer-ics2024 <tracer@njuics.org>
2025-01-19 18:14:43 +0800

    >  compile NEMU 231250160 左涛 Linux zeet-XiaoXinPro-16-APH8 6.11.0-13-generic #14-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov 30 23:51:51 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:14:43 up  8:30,  2 users,  load average: 1.36, 1.15, 0.92

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -25,13 +25,9 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
       int src_index = (src_y + y) * src->w + src_x + x;
       int dst_index = (dst_y + y) * dst->w + dst_x + x;
       memcpy(&dst_pixels[dst_index], &src_pixels[src_index], bpp);
+      printf("%x ", *(uint32_t *)(&dst_pixels[dst_index]));
     }
   }
-  //打印dst_pixels的内容
-  // for (int i = 0; i < dst->w * dst->h * 4; i++) {
-  //   printf("%x ", dst_pixels[i]);
-  // }
-  printf("bpp=%d\n", bpp);
 }
 
 void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
