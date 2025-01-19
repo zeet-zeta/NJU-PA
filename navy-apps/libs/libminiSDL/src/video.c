14e6fbe891fcebdc1844e00fb791c3db03a4db19
tracer-ics2024 <tracer@njuics.org>
2025-01-19 19:02:07 +0800

    >  compile NEMU 231250160 左涛 Linux zeet-XiaoXinPro-16-APH8 6.11.0-13-generic #14-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov 30 23:51:51 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:02:07 up  9:17,  2 users,  load average: 1.52, 0.93, 0.60

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-//复制src的内容到dst，兼容不同的bpp
+//复制src的内容到dst
 void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_Rect *dstrect) {
   assert(dst && src);
   assert(dst->format->BitsPerPixel == src->format->BitsPerPixel);
@@ -18,14 +18,14 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
   int dst_y = dstrect == NULL ? 0 : dstrect->y;
   
   int bpp = src->format->BytesPerPixel;
-  uint8_t *src_pixels = src->pixels;
-  uint8_t *dst_pixels = dst->pixels;
+  uint32_t *src_pixels = (uint32_t *)src->pixels;
+  uint32_t *dst_pixels = (uint32_t *)dst->pixels;
   for (int y = 0; y < src_h; y++) {
     for (int x = 0; x < src_w; x++) {
       int src_index = (src_y + y) * src->w + src_x + x;
       int dst_index = (dst_y + y) * dst->w + dst_x + x;
       //中括号是一个语法糖，跳动的距离还是依照指针的类型
-      memcpy(dst + dst_index * bpp, src + src_index * bpp, bpp);
+      memcpy(dst + dst_index, src + src_index, bpp);
     }
   }
 }
