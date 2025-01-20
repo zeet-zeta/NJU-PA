0214c01fb3feb72a4365c4fce3f2a3f9c065b90d
tracer-ics2024 <tracer@njuics.org>
2025-01-20 21:27:13 +0800

    >  compile NEMU 231250160 左涛 Linux zeet-XiaoXinPro-16-APH8 6.11.0-13-generic #14-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov 30 23:51:51 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:27:13 up 10:34,  2 users,  load average: 0.66, 0.74, 0.78

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -68,12 +68,14 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
   int envc = 0;
   while (envp[envc]) envc++;
 
-  // uintptr_t ustack_end = (uintptr_t)new_page(8);
-  uintptr_t ustack_end = (uintptr_t)heap.end;
+  uintptr_t ustack_end = (uintptr_t)new_page(8);
+  // uintptr_t ustack_end = (uintptr_t)heap.end;
   uintptr_t ustack_top = ustack_end;
-  char **argv_copy = malloc(argc * sizeof(char *));
-  char **envp_copy = malloc(envc * sizeof(char *));
-  printf("%p %p", argv_copy, envp_copy);
+  //此处不能使用malloc,其中一个原因是malloc和new_page分配的空间是冲突的
+  // char **argv_copy = malloc(argc * sizeof(char *));
+  // char **envp_copy = malloc(envc * sizeof(char *));
+  char *argv_copy[argc];
+  char *envp_copy[envc];
 
   for (int i = envc - 1; i >= 0; i--) {
     ustack_top -= strlen(envp[i]) + 1; // '\0'
@@ -95,6 +97,4 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
   ustack_top -= sizeof(int);
   *(int *)ustack_top = argc;
   pcb->cp->GPRx = ustack_top;
-  free(argv_copy);
-  free(envp_copy);
 }
