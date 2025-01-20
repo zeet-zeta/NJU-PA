83872841502b126e0d9910037553f38c23414f97
tracer-ics2024 <tracer@njuics.org>
2025-01-20 17:34:31 +0800

    >  compile NEMU 231250160 左涛 Linux zeet-XiaoXinPro-16-APH8 6.11.0-13-generic #14-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov 30 23:51:51 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:34:31 up  6:41,  2 users,  load average: 1.33, 1.06, 0.96

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -7,12 +7,10 @@ int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
 void call_main(uintptr_t *args) {
   int argc = (int)args[0];
-  char **argv = (char **)(args + 1);
-  char **envp = (char **)(args + 1 + argc + 1);
-  printf("argc = %d\n", argc);
-  for (int i = 0; i < argc; i++) {
-    printf("argv[%d] = %s\n", i, argv[i]);
-  }
+  // char **argv = (char **)(args + 1);
+  // char **envp = (char **)(args + 1 + argc + 1);
+  char **argv = (char **)args[1];
+  char **envp = (char **)args[argc + 2];
   environ = envp;
   exit(main(argc, argv, envp));
   assert(0);
