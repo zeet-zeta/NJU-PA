34342b8d788fdae16f6ccffc7cbf6fb3e7811440
tracer-ics2024 <tracer@njuics.org>
2025-01-20 20:05:41 +0800

    >  compile NEMU 231250160 左涛 Linux zeet-XiaoXinPro-16-APH8 6.11.0-13-generic #14-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov 30 23:51:51 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:05:41 up  9:12,  2 users,  load average: 1.06, 1.02, 0.98

--- navy-apps/tests/exec-test/main.c
+++ navy-apps/tests/exec-test/main.c
@@ -4,7 +4,7 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
-  printf("Hello from exec-test %d\n", getpid());
+  putchar('a');
   int n = (argc >= 2 ? atoi(argv[1]) : 1);
   printf("%s: argv[1] = %d\n", argv[0], n);
 
