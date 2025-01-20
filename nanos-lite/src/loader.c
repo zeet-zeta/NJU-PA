3d621dfa11a805af8a1dd54fe68df0316e9f02b4
tracer-ics2024 <tracer@njuics.org>
2025-01-20 22:35:06 +0800

    >  compile NEMU 231250160 左涛 Linux zeet-XiaoXinPro-16-APH8 6.11.0-13-generic #14-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov 30 23:51:51 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:35:06 up 36 min,  2 users,  load average: 1.79, 1.36, 1.13

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -75,7 +75,7 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
   //此处不能使用malloc,其中一个原因是malloc和new_page分配的空间是冲突的
   char *argv_copy[argc];
   char *envp_copy[envc];
-
+  printf("here\n");
   for (int i = envc - 1; i >= 0; i--) {
     ustack_top -= strlen(envp[i]) + 1; // '\0'
     strcpy((char *)ustack_top, envp[i]);
@@ -87,7 +87,7 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
     argv_copy[i] = (char *)ustack_top;
   }
 
-  printf("here\n");
+
 
   ustack_top -= (envc + 1) * sizeof(char *);
   memcpy((void *)ustack_top, envp_copy, envc * sizeof(char *));
