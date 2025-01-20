da30fcd03749a18054a0f01876ee455c35324912
tracer-ics2024 <tracer@njuics.org>
2025-01-20 14:02:19 +0800

    >  compile NEMU 231250160 左涛 Linux zeet-XiaoXinPro-16-APH8 6.11.0-13-generic #14-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov 30 23:51:51 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:02:19 up  3:09,  2 users,  load average: 1.40, 1.04, 0.94

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -45,7 +45,7 @@ bool cte_init(Context*(*handler)(Event, Context*)) {
 Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
   //Area [start, end)
   Context *c = (Context *)(kstack.end - sizeof(Context));
-  c->mepc = (uintptr_t)entry - 4;
+  c->mepc = (uintptr_t)entry + 4;
   c->mstatus = 0x1800;
   c->GPR2 = (uintptr_t)arg; //给f函数传参
   return c;
