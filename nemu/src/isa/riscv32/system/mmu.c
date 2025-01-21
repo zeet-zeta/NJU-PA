de9ed44a966c48f2166eb30289c0a56848817900
tracer-ics2024 <tracer@njuics.org>
2025-01-21 23:43:04 +0800

    >  compile NEMU 231250160 左涛 Linux zeet-XiaoXinPro-16-APH8 6.11.0-13-generic #14-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov 30 23:51:51 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  23:43:04 up 13:50,  2 users,  load average: 0.81, 0.92, 0.83

--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -17,17 +17,49 @@
 #include <memory/vaddr.h>
 #include <memory/paddr.h>
 
-#define PTE_V 0x1
+// #define PTE_V 0x1
+
+// paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
+//   word_t satp = cpu.satp;
+//   paddr_t root_ppn = satp << 12;
+//   paddr_t first_pte_addr = root_ppn + ((vaddr >> 22) << 2);
+//   word_t first_pte = paddr_read(first_pte_addr, 4);
+//   Assert(first_pte & PTE_V, "vaddr = %x first_pte = %x root_ppn = %x", vaddr, first_pte, root_ppn);
+//   paddr_t second_pte_addr = (first_pte >> 10 << 12) + ((vaddr >> 12 & 0x3ff) << 2);
+//   word_t second_pte = paddr_read(second_pte_addr, 4);
+//   assert(second_pte & PTE_V);
+//   paddr_t paddr = (second_pte >> 10 << 12) + (vaddr & 0xfff);
+//   return paddr;
+// }
+
+typedef uint32_t PTE;
+#define PTE_V 0x01
+#define PTE_R 0x02
+#define PTE_W 0x04
+#define PTE_X 0x08
+#define PTE_U 0x10
+#define PTE_A 0x40
+#define PTE_D 0x80
+
+#define PTE_PPN_MASK (0xFFFFFC00u)
+#define PTE_PPN(x) (((x) & PTE_PPN_MASK) >> 10)
+#define PGT1_ID(val) (val >> 22)
+#define PGT2_ID(val) ((val & 0x3fffff) >> 12)
+#define OFFSET(val) (val & 0xfff)
 
 paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
-  word_t satp = cpu.satp;
-  paddr_t root_ppn = satp << 12;
-  paddr_t first_pte_addr = root_ppn + ((vaddr >> 22) << 2);
-  word_t first_pte = paddr_read(first_pte_addr, 4);
-  Assert(first_pte & PTE_V, "vaddr = %x first_pte = %x root_ppn = %x", vaddr, first_pte, root_ppn);
-  paddr_t second_pte_addr = (first_pte >> 10 << 12) + ((vaddr >> 12 & 0x3ff) << 2);
-  word_t second_pte = paddr_read(second_pte_addr, 4);
-  assert(second_pte & PTE_V);
-  paddr_t paddr = (second_pte >> 10 << 12) + (vaddr & 0xfff);
-  return paddr;
+    paddr_t pte_1_addr = (cpu.satp << 12) + PGT1_ID(vaddr) * 4;   // 每个表项占 4 个字节，计算得到虚拟地址的一级页面表项地址
+    PTE pte_1 = paddr_read(pte_1_addr, sizeof(PTE));
+    Assert(pte_1 & PTE_V, "first class pte is not valid, vaddr=%#x", vaddr);
+
+    paddr_t pte_2_addr = (PTE_PPN(pte_1) << 12) + PGT2_ID(vaddr) * 4; // 高 22 位是 PPN，physical page number, 物理页号. 但是在 4KB 页面的设置下，高两位被直接移出去了
+    PTE pte_2 = paddr_read(pte_2_addr, sizeof(PTE));
+    Assert(pte_2 & PTE_V, "second class pte is not valid, vaddr=%#x", vaddr);
+
+    // 记录访问、写入标志。0 是取指，1 是读取，2 是
+    paddr_write(pte_2_addr, 4, type == MEM_TYPE_WRITE ? pte_2 | PTE_A | PTE_D : pte_2 | PTE_A);
+
+    paddr_t pa = PTE_PPN(pte_2) << 12 | OFFSET(vaddr);
+    //Assert(pa == vaddr, "get physical address wrong, pa=%#x, va=%#x", pa, vaddr);
+    return pa;
 }
