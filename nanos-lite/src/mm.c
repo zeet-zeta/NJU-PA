4faa2d440409b774d4ac9a1d3e42cd20d96522e6
tracer-ics2024 <tracer@njuics.org>
2025-01-21 22:32:37 +0800

    >  compile NEMU 231250160 左涛 Linux zeet-XiaoXinPro-16-APH8 6.11.0-13-generic #14-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov 30 23:51:51 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:32:37 up 12:39,  2 users,  load average: 0.49, 0.50, 0.70

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -28,7 +28,7 @@ int mm_brk(uintptr_t brk) {
 }
 
 void init_mm() {
-  pf = (void *)ROUNDUP(heap.start, PGSIZE) + 4 * PGSIZE;
+  pf = (void *)ROUNDUP(heap.start, PGSIZE);
   Log("free physical pages starting from %p", pf);
 
 #ifdef HAS_VME
