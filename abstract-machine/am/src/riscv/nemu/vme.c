61f511480fb38224793a9cc0a203c8a9d5fc6580
tracer-ics2024 <tracer@njuics.org>
2025-01-22 22:29:03 +0800

    >  compile NEMU 231250160 左涛 Linux zeet-XiaoXinPro-16-APH8 6.11.0-13-generic #14-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov 30 23:51:51 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:29:03 up  1:40,  2 users,  load average: 0.87, 0.88, 0.93

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -21,8 +21,10 @@ static inline void set_satp(void *pdir) {
 }
 
 static inline uintptr_t get_satp() {
+  
   uintptr_t satp;
   asm volatile("csrr %0, satp" : "=r"(satp));
+  printf("get_satp: %p\n", satp << 12);
   return satp << 12;
 }
 
