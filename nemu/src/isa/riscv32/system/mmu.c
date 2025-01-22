8a935c4afbcc7a86c969c3ea47cd997f0c5609c6
tracer-ics2024 <tracer@njuics.org>
2025-01-22 09:24:46 +0800

    >  compile NEMU 231250160 左涛 Linux zeet-XiaoXinPro-16-APH8 6.11.0-13-generic #14-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov 30 23:51:51 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  09:24:46 up  1:03,  2 users,  load average: 0.76, 0.63, 0.65

--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -22,12 +22,12 @@
 paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
   word_t satp = cpu.satp;
   paddr_t root_ppn = satp << 12;
-  paddr_t first_pte_addr = root_ppn + ((vaddr >> 22) << 2);
+  paddr_t first_pte_addr = root_ppn + (vaddr >> 22) * 4;
   word_t first_pte = paddr_read(first_pte_addr, 4);
   Assert(first_pte & PTE_V, "vaddr = %x first_pte = %x root_ppn = %x", vaddr, first_pte, root_ppn);
-  paddr_t second_pte_addr = (first_pte >> 10 << 12) + ((vaddr >> 12 & 0x3ff) << 2);
+  paddr_t second_pte_addr = (first_pte & ~0xfff) + ((vaddr >> 12 & 0x3ff) * 4);
   word_t second_pte = paddr_read(second_pte_addr, 4);
   assert(second_pte & PTE_V);
-  paddr_t paddr = (second_pte >> 10 << 12) + (vaddr & 0xfff);
+  paddr_t paddr = (second_pte & ~0xfff) + (vaddr & 0xfff);
   return paddr;
 }
