48ed84ddeab4b93925740f3f32db04e520942e73
tracer-ics2024 <tracer@njuics.org>
2025-01-22 22:02:27 +0800

    >  compile NEMU 231250160 左涛 Linux zeet-XiaoXinPro-16-APH8 6.11.0-13-generic #14-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov 30 23:51:51 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:02:27 up  1:13,  2 users,  load average: 0.82, 0.72, 0.74

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -49,7 +49,6 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 //把buf中的内容写到屏幕上，可以和am中gpu.c比较
 //这里的x和y是屏幕参数
 size_t fb_write(const void *buf, size_t offset, size_t len) {
-  yield();
   int pixels_num = offset / 4;
   int x = pixels_num % w;
   int y = pixels_num / w;
