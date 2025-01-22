8f14d16bb20b7d4ad76c152b73aeebeea5c8a0b2
tracer-ics2024 <tracer@njuics.org>
2025-01-22 22:41:41 +0800

    >  compile NEMU 231250160 左涛 Linux zeet-XiaoXinPro-16-APH8 6.11.0-13-generic #14-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov 30 23:51:51 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:41:41 up  1:52,  2 users,  load average: 0.60, 0.88, 0.93

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -10,17 +10,8 @@ extern void context_uload(PCB *pcb, const char *filename, char *const argv[], ch
 extern void naive_uload(PCB *pcb, const char *filename);
 
 void hello_fun(void *arg) {
-  int j = 1;
   printf("Hello! ");
-  while (1) {
-    //bug的原因在于我尝试*(int *)arg，就会将读取0x00000001的内存，而不是arg的值
-    // printf("Hello! ");
-    j ++;
-    if (j % 100000 == 0) {
-      printf("Hello! ");
-    }
-    yield();
-  }
+  yield();
 }
 void context_kload(PCB *pcb, void (*entry)(void *), void *arg) {
   pcb->cp = kcontext((Area){pcb->stack, pcb + 1}, entry, arg);
