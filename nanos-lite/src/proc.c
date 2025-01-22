c526f0f89674224e78a59fd00e896cd106747e6d
tracer-ics2024 <tracer@njuics.org>
2025-01-22 13:27:09 +0800

    >  compile NEMU 231250160 左涛 Linux zeet-XiaoXinPro-16-APH8 6.11.0-13-generic #14-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov 30 23:51:51 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:27:09 up  5:06,  2 users,  load average: 0.97, 0.76, 0.67

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -26,6 +26,7 @@ void context_kload(PCB *pcb, void (*entry)(void *), void *arg) {
 }
 
 Context *schedule(Context *prev) {
+  printf("schedule\n");
   current->cp = prev;
   // current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
   current = &pcb[0];
@@ -39,10 +40,10 @@ void switch_boot_pcb() {
 void init_proc() {
   // context_kload(&pcb[0], hello_fun, (void *)1);
   
-  char *argv_example[] = {"/bin/bird", NULL};
+  char *argv_example[] = {"/bin/pal", NULL};
   char *envp_example[] = {NULL};
   //数组的指针本身是常量，不能改变指向的地址，但指针指向的结果（字符串内容）可以是常量或非常量，具体取决于如何定义
-  context_uload(&pcb[0], "/bin/bird", argv_example, envp_example);
+  context_uload(&pcb[0], "/bin/pal", argv_example, envp_example);
   switch_boot_pcb();
 
   Log("Initializing processes...");
